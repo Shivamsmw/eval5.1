53beeac707696e3b46e66756f3363d9cf7e373fe
Shivamsmw <92774582+Shivamsmw@users.noreply.github.com>
2022-03-14 21:47:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Wishlist from "./Components/Wishlist";
 import './App.css';
+import Vegetables from "./Components/Vegetables";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Wishlist />
+      <Vegetables />
     </div>
   );
 }
